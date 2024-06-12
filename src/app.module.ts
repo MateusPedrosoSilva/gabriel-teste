1138263cecc62a78808be70feb1c2447bbec9b4d
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2024-06-11 23:07:26 -0300

    feat(typeorm): add and configure typeorm. And remove the standart route from Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      database: 'gabriel_cameras_db',
+      username: 'gabriel',
+      password: 'Gabriel123',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
