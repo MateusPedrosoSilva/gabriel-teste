6f7eab3a4d909347422be8cf52570a978ce02055
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2024-06-15 12:38:24 -0300

    feat(test): add test for typeorm repository

--- src/@core/domain/user.spec.ts
+++ src/@core/domain/user.spec.ts
@@ -1,9 +1,12 @@
 import { User } from './user';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('User Unit Test', () => {
   it('should create a new user', () => {
-    const user = new User('123', 'user-1');
-    expect(user.id).toBe('123');
-    expect(user.name).toBe('user-1');
+    const id = uuidv4();
+    const name = 'Mateus Pedroso';
+    const user = new User(id, name);
+    expect(user.id).toBe(id);
+    expect(user.name).toBe(name);
   });
 });
--- src/@core/infra/db/typeorm/entities/user.entity.ts
+++ src/@core/infra/db/typeorm/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('users')
 export class UserTypeormEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
